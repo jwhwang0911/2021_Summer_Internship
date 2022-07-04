a28b482b32d3db437fa207268c18421ca85b4656
cglab <star4007lg@gmail.com>
2022-07-04 14:27:19 +0900

    22/07/04

---
+++ Raytracing/build/CMakeFiles/inOneWeekend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inOneWeekend.dir/src/InOneWeekend/main.cc.o"
+  "inOneWeekend.pdb"
+  "inOneWeekend"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inOneWeekend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
