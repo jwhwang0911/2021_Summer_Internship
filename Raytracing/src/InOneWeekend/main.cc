b24a8a05a336d19df7ee4575366125fda8bebae2
cglab <star4007lg@gmail.com>
2022-07-05 10:06:29 +0900

    #6.3 Hittable Object and Polymorphism of Sphere

--- Raytracing/src/InOneWeekend/main.cc
+++ Raytracing/src/InOneWeekend/main.cc
@@ -41,9 +41,9 @@ int main() {
 
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    auto focal_length = 0.5;
 
-    auto origin = point3(0, 0, 0);
+    auto origin = point3(0, 0, -0.4);
     auto horizontal = vec3(viewport_width, 0, 0);
     auto vertical = vec3(0, viewport_height, 0);
     auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
