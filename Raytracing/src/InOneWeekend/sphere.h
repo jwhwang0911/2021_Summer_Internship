05db179f528c3668d1cb9c625ce08edc779c1293
cglab <star4007lg@gmail.com>
2022-07-05 11:14:45 +0900

    #6 Render Hittable Objects and APIs

--- Raytracing/src/InOneWeekend/sphere.h
+++ Raytracing/src/InOneWeekend/sphere.h
@@ -17,7 +17,7 @@ class sphere : public hittable {
         double radius;
 };
 
-bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+bool sphere::hit (const ray& r, double t_min, double t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
@@ -40,7 +40,8 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     
     rec.t = root;
     rec.p = r.at(rec.t);
-    rec.normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
 
     return true;
 }
